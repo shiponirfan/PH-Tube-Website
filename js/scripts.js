ca0e6147c258837c1197bdd7a77e37cb0569ae2c
shiponirfan <imshiponirfan@gmail.com>
2023-09-02 16:37:42 +0600

    Time Hours Section Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,23 @@
+// Main Categories Function
 const handlerCategories = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
     handlerTabCategories(data);
 };
-
+handlerCategories();
+// Tab Button Function
 const handlerTabCategories = (categories) => {
     const tabContainer = document.getElementById('tab-container');
     categories.data.forEach(category => {
         // console.log(category.category);
         const span = document.createElement('span');
         span.innerHTML = `
-        <a onclick="handlerShowCategories('${category.category_id}')" class="tab btn mx-2 normal-case hover:bg-[#FF1F3D] hover:text-white">${category.category}</a> 
+        <a onclick="handlerShowCategories('${category.category_id}')" class="tab btn m-2 normal-case hover:bg-[#FF1F3D] hover:text-white">${category.category}</a> 
         `;
         tabContainer.appendChild(span);
     });
 };
-
-const handlerAllCategoriesActive = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
-    const data = await response.json();
-    const cardContainer = document.getElementById('card-container');
-    cardContainer.innerHTML = '';
-    data.data.forEach(categories =>{
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <figure><img class="rounded-lg h-52 w-full" src="${categories.thumbnail}" alt="image" /></figure>
-        <div class="flex mt-5">
-            <div class="avatar mr-3">
-                <div class="w-10 h-10 rounded-full">
-                    <img src="${categories.authors[0].profile_picture}" />
-                </div>
-                </div>
-          <div>
-            <h2 class="card-title">${categories.title}</h2>
-          <p class="flex items-center my-2">${categories.authors[0].profile_name} <span class="ml-2 ${categories.authors[0]?.verified? 'block' : 'hidden'}"><img src="./images/blue-tick.png" alt="blue tick"></span></p>
-          <p>${categories.others.views}</p>
-          </div>
-        </div>
-        `;
-        cardContainer.appendChild(div);
-    })
-}
-handlerAllCategoriesActive();
-
+// Show Categories Function
 const handlerShowCategories = async (categories_id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categories_id}`);
     const data = await response.json();
@@ -50,9 +25,15 @@ const handlerShowCategories = async (categories_id) => {
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
     data.data.forEach(categories =>{
+        const posted_date =  postedDate(categories.others.posted_date);
         const div = document.createElement('div');
         div.innerHTML = `
-        <figure><img class="rounded-lg h-52 w-full" src="${categories.thumbnail}" alt="image" /></figure>
+        <figure class="relative">
+        <div class="${categories.others?.posted_date? 'block' : 'hidden'} absolute bottom-3 right-3 bg-[#171717] rounded-lg">
+            <h3 class="text-white p-2 ">${posted_date}</h3>
+        </div>
+                <img class="rounded-lg h-52 w-full" src="${categories.thumbnail}" alt="image" />
+            </figure>
         <div class="flex mt-5">
             <div class="avatar mr-3">
                 <div class="w-10 h-10 rounded-full">
@@ -68,7 +49,6 @@ const handlerShowCategories = async (categories_id) => {
         `;
         cardContainer.appendChild(div);
     });
-
     const noContentContainer = document.getElementById('no-content-container');
     if(!data.status){
         noContentContainer.innerHTML = '';
@@ -81,8 +61,14 @@ const handlerShowCategories = async (categories_id) => {
         noContentContainer.appendChild(div);
     }else{
         noContentContainer.innerHTML = '';
-    }
-
-}
-
-handlerCategories();
+    };
+};
+// Posted Date Function
+const postedDate = (time) => {
+    time = Math.floor(time / 60);
+    let hours = Math.floor(time/60);
+    let minutes = time % 60;
+    return `${hours}hrs ${minutes} min ago`
+};
+// All Categories Active Function Call
+handlerShowCategories(1000);
