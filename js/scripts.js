ecada4893101f893e741d884ee7688b1f99911d3
shiponirfan <imshiponirfan@gmail.com>
2023-09-02 12:15:50 +0600

    Navbar Section Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log("PH Tube");
+const handlerCategories = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    console.log(data.data);
+};
+
+handlerCategories();
