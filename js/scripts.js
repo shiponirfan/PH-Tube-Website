3d891bd43d059cab3cdc269d202daa2c7bed1f2f
shiponirfan <imshiponirfan@gmail.com>
2023-09-02 12:39:10 +0600

    Tab Section Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 const handlerCategories = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
-    console.log(data.data);
+    handlerShowCategories(data);
 };
+
+const handlerShowCategories = (categories) => {
+    const tabContainer = document.getElementById('tab-container');
+    categories.data.forEach(category => {
+        console.log(category.category);
+        const span = document.createElement('span');
+        span.innerHTML = `
+        <a class="tab btn mx-2 normal-case hover:bg-[#FF1F3D] hover:text-white">${category.category}</a> 
+        `;
+        tabContainer.appendChild(span);
+    });
+}
 
 handlerCategories();
