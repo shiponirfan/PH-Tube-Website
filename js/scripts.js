86a6281988e7c639948b84e0beb709a24a06069d
shiponirfan <imshiponirfan@gmail.com>
2023-09-02 21:35:15 +0600

    Sort Function Added

--- js/scripts.js
+++ js/scripts.js
@@ -84,3 +84,65 @@ const postedDate = (time) => {
 };
 // All Categories Active Function Call
 handlerShowCategories(1000);
+// Sort By View Function
+const sortByView = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/category/1000"
+  );
+  const data = await response.json();
+  // Remove K Form data.data.array.others.views
+  data.data.forEach((removeK) => {
+    removeK.others.views = removeK.others.views.slice(0, -1);
+  });
+  // Sort Data From data.data.array.others.views
+  data.data.sort((a, b) => b.others.views - a.others.views);
+
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  data.data.forEach((categories) => {
+    const posted_date = postedDate(categories.others.posted_date);
+    const div = document.createElement("div");
+    div.innerHTML = `
+          <figure class="relative">
+          <div class="${
+            categories.others?.posted_date ? "block" : "hidden"
+          } absolute bottom-3 right-3 bg-[#171717] rounded-lg">
+              <h3 class="text-white p-2 ">${posted_date}</h3>
+          </div>
+                  <img class="rounded-lg h-52 w-full" src="${
+                    categories.thumbnail
+                  }" alt="image" />
+              </figure>
+          <div class="flex mt-5">
+              <div class="avatar mr-3">
+                  <div class="w-10 h-10 rounded-full">
+                      <img src="${categories.authors[0].profile_picture}" />
+                  </div>
+                  </div>
+            <div>
+              <h2 class="card-title">${categories.title}</h2>
+            <p class="flex items-center my-2">${
+              categories.authors[0].profile_name
+            } <span class="ml-2 ${
+      categories.authors[0]?.verified ? "block" : "hidden"
+    }"><img src="./images/blue-tick.png" alt="blue tick"></span></p>
+            <p>${categories.others.views}K</p>
+            </div>
+          </div>
+          `;
+    cardContainer.appendChild(div);
+  });
+  const noContentContainer = document.getElementById("no-content-container");
+  if (!data.status) {
+    noContentContainer.innerHTML = "";
+    const div = document.createElement("div");
+    div.classList = "flex flex-col justify-center items-center";
+    div.innerHTML = `
+          <img src="./images/Icon.png" alt="icon">
+          <h2 class="font-bold text-3xl text-center mt-8">Oops!! Sorry, There is no <br> content here</h2>
+          `;
+    noContentContainer.appendChild(div);
+  } else {
+    noContentContainer.innerHTML = "";
+  }
+};
